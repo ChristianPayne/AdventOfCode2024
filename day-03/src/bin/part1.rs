9c3b986255b04c12ee8417c67c25f6239f7900f5
Christian Payne <christianpayne522@gmail.com>
2024-12-02 23:48:26 -0800

    Day 03 Part 1 complete!

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -1,4 +1,13 @@
-use nom::{bytes::complete::tag, Err, IResult, Parser};
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take_while, take_while1},
+    character::complete::{self, anychar},
+    combinator::{map, map_res, opt},
+    error,
+    multi::many0,
+    sequence::{preceded, separated_pair, tuple},
+    IResult,
+};
 
 fn main() {
     let input = include_str!("./input.txt");
@@ -6,16 +15,40 @@ fn main() {
     dbg!(output);
 }
 
-fn part_1(input: &str) -> i32 {
-    todo!()
+fn part_1(input: &str) -> u32 {
+    let (_, result) = parse_all_mul(input).unwrap();
+
+    dbg!(&result);
+
+    result.iter().fold(0, |acc, (x, y)| acc + (x * y))
 }
 
-fn parse_mul(input: &str) -> IResult<&str, (i32, i32)> {
-    let (input, _) = tag("mul")(input)?;
-    let (input, x) = nom::character::complete::i32(input)?;
-    let (input, _) = tag(",")(input)?;
-    let (input, y) = nom::character::complete::i32(input)?;
+fn parse_all_mul(input: &str) -> IResult<&str, Vec<(u32, u32)>> {
+    many0(parse_mul_or_skip)(input).map(|(next_input, results)| {
+        // Filter out `None` results
+        (next_input, results.into_iter().flatten().collect())
+    })
+}
+
+fn parse_mul_or_skip(input: &str) -> IResult<&str, Option<(u32, u32)>> {
+    alt((
+        map(parse_mul, Some),           // Parse valid "mul"
+        map(consume_invalid, |_| None), // Consume one invalid character
+    ))(input)
+}
+
+fn parse_mul(input: &str) -> IResult<&str, (u32, u32)> {
+    let (input, (_, (x, y), _)) = tuple((tag("mul("), parse_numbers, tag(")")))(input)?;
+
     Ok((input, (x, y)))
+}
+
+fn consume_invalid(input: &str) -> IResult<&str, ()> {
+    map(anychar, |_| ())(input)
+}
+
+fn parse_numbers(input: &str) -> IResult<&str, (u32, u32)> {
+    separated_pair(complete::u32, tag(","), complete::u32)(input)
 }
 
 #[cfg(test)]
@@ -23,8 +56,19 @@ mod tests {
     use super::*;
 
     #[test]
+    fn parse_numbers_test() {
+        let (_, result) = parse_numbers("2,4").unwrap();
+        assert_eq!(result, (2, 4));
+    }
+    #[test]
+    fn parse_mul_test() {
+        let (_, result) = parse_mul("mul(2,4)").unwrap();
+        assert_eq!(result, (2, 4));
+    }
+    #[test]
     fn part_1_sample() {
-        let result = part_1("");
-        assert_eq!(result, 2);
+        let result =
+            part_1("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))");
+        assert_eq!(result, 161);
     }
 }
