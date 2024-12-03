0f1bcba8088dd954970d2df6b32ffbdf320bc58b
Christian Payne <christianpayne522@gmail.com>
2024-12-02 21:11:40 -0800

    Day 02 part 02 incomplete.

--- README.md
+++ README.md
@@ -4,5 +4,10 @@
 `cargo new --lib day-00` at the root of the repo and cargo will scaffold a new library.  
 Add a new `bin` folder inside of the `src` folder and put the `part1.rs`, `part2.rs` and `input.txt` files in that.
 ## Running a day
-While inside of a day, run `cargo test` to execute all of the tests in that workspace.  
-`cargo test --bin part1` will run just a certain part's tests.
+Make sure to `cd` into the day you want to run.
+### Testing sample data
+Run `cargo test` to execute all of the tests in that workspace.  
+`cargo test --bin part1` will run just a certain part's tests.  
+Adding the `-- --nocapture` flags on tests will allow print and debug statements into the terminal.
+### Running real input data
+Run `cargo run --bin part1` to run a specific bin. There is no "run all" command because we are not compiling binaries.
--- day-02/src/bin/part2.rs
+++ day-02/src/bin/part2.rs
@@ -1,3 +1,5 @@
+use std::{cmp::Ordering, collections::HashMap};
+
 fn main() {
     let input = include_str!("./input.txt");
     let output = part_2(input);
@@ -5,7 +7,9 @@ fn main() {
 }
 
 fn part_2(input: &str) -> i32 {
+    println!("START TEST");
     input.split('\n').fold(0, |total_safe_reports, report| {
+        println!("---");
         let levels: Vec<i32> = report
             .split(' ')
             .map(|level| level.parse::<i32>().expect("Failed to parse level"))
@@ -19,79 +23,138 @@ fn part_2(input: &str) -> i32 {
     })
 }
 
-fn test_report(levels: Vec<i32>) -> bool {
+enum ProblemDampener {
+    Ready,
+    Used,
+}
+fn test_report(mut levels: Vec<i32>) -> bool {
+    let mut problem_dampener = ProblemDampener::Ready;
     // Test for all increasing or all decreasing
-    // Test for no more than 3 level changes.
-    if !test_direction(levels.clone()) || !test_distance(levels.clone()) {
-        return false;
+    loop {
+        match (test_direction(&levels), &problem_dampener) {
+            // Didn't fail, move onto the next test.
+            (None, _) => (),
+            // Failed but we have the dampener. Remove the bad level and start the tests over again.
+            (Some(fail_point), ProblemDampener::Ready) => {
+                levels.remove(fail_point);
+                problem_dampener = ProblemDampener::Used;
+                continue;
+            }
+            (Some(_), ProblemDampener::Used) => {
+                return false;
+            }
+        }
+
+        // Test for no more than 3 level changes.
+        match (test_distance(&levels), &problem_dampener) {
+            // Didn't fail, break out of the loop. We passed all tests.
+            (None, _) => break,
+            // Failed but we have the dampener. Remove the bad level and start the tests over again.
+            (Some(fail_point), ProblemDampener::Ready) => {
+                levels.remove(fail_point);
+                problem_dampener = ProblemDampener::Used;
+                continue;
+            }
+            (Some(_), ProblemDampener::Used) => {
+                return false;
+            }
+        }
     }
 
     true
 }
 
+#[derive(Eq, Hash, PartialEq, Debug, PartialOrd, Ord)]
 enum Direction {
     None,
     Increasing,
     Decreasing,
 }
-fn test_direction(levels: Vec<i32>) -> bool {
-    let mut current_direction: Direction = Direction::None;
-    for window in levels.windows(2) {
+fn test_direction(levels: &[i32]) -> Option<usize> {
+    let report_direction = levels
+        .windows(2)
+        .fold(HashMap::<Direction, i32>::new(), |mut acc, window| {
+            let current_level = window[0];
+            let next_level = window[1];
+
+            *acc.entry(get_direction(current_level, next_level))
+                .or_default() += 1;
+
+            acc
+        })
+        .into_iter()
+        .max_by_key(|(_, v)| *v)
+        .map(|(k, _)| k)
+        .unwrap();
+
+    for (index, window) in levels.windows(2).enumerate() {
         let current_level = window[0];
         let next_level = window[1];
 
-        let window_safe = match (current_level > next_level, &current_direction) {
-            // We don't have a direction yet. Set one and move on.
-            (decreasing, Direction::None) => {
-                current_direction = if decreasing {
-                    Direction::Decreasing
-                } else {
-                    Direction::Increasing
-                };
-
-                true
-            }
-            // We are switching direction. No bueno.
-            (true, Direction::Increasing) => false,
-            (false, Direction::Decreasing) => false,
-            // We aren't in a special case, you may pass.
-            _ => true,
+        let window_is_safe = if current_level == next_level {
+            false
+        } else {
+            report_direction == get_direction(current_level, next_level)
         };
 
         // Early out from the function if we are not safe.
-        if !window_safe {
-            return false;
+        if !window_is_safe {
+            println!("Failed direction on {} index: {:?}", index, levels);
+
+            if get_direction(current_level, next_level) == report_direction {
+                return Some(index + 1);
+            } else {
+                return Some(index);
+            }
         }
     }
 
     // We must be good at this point
-    true
+    println!("Passed direction {:?}", levels);
+    None
 }
 
-fn test_distance(levels: Vec<i32>) -> bool {
-    for window in levels.windows(2) {
+fn get_direction(current_level: i32, next_level: i32) -> Direction {
+    match current_level.cmp(&next_level) {
+        Ordering::Greater => Direction::Decreasing,
+        Ordering::Less => Direction::Increasing,
+        Ordering::Equal => Direction::None,
+    }
+}
+
+fn test_distance(levels: &[i32]) -> Option<usize> {
+    for (index, window) in levels.windows(2).enumerate() {
         let level1 = window[0];
         let level2 = window[1];
 
         // Can't have the same levels on a report.
         if level1 == level2 {
-            return false;
+            println!("Failed distance (same) on {} index: {:?}", index, levels);
+            return Some(index);
         }
 
         // Make sure the distance is not greater than 3.
         if (level1 - level2).abs() > 3 {
-            return false;
+            println!("Failed distance on {} index: {:?}", index, levels);
+            return Some(index);
         }
     }
 
-    true
+    println!("Passed distance {:?}", levels);
+    None
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
+    // #[test]
+    fn part_2_tests() {
+        part_2_sample();
+        part_2_reddit();
+    }
+
+    // #[test]
     fn part_2_sample() {
         let result = part_2(
             "7 6 4 2 1
@@ -101,6 +164,31 @@ mod tests {
 8 6 4 4 1
 1 3 6 7 9",
         );
-        assert_eq!(result, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn part_2_reddit() {
+        let result = part_2(
+            "48 46 47 49 51 54 56
+1 1 2 3 4 5
+1 2 3 4 5 5
+5 1 2 3 4 5
+1 4 3 2 1
+1 6 7 8 9
+1 2 3 4 3
+9 8 7 6 7
+7 10 8 10 11
+29 28 27 25 26 25 22 20",
+        );
+        assert_eq!(result, 10);
     }
 }
+
+// Too Low - 402
+// Not the right answer - 404
+// Not the right answer - 406
+// Not the right answer - 408
+// Not the right answer - 410
+// Too High - 440
+// Too High - 649
